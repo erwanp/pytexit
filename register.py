b2302d0afbb62a72733cc2f36bbbfca5652b6b84
Erwan Pannier <erwan.pannier@gmail.com>
2018-01-26 21:29:34 +0100

    updated for pypi

--- register.py
+++ register.py
@@ -14,16 +14,25 @@ Requirement
 
 from __future__ import print_function, absolute_import
 import os
+import shutil
 
+package_name = 'pytexit'
+
+# All the rest below is standard:
 
 if os.path.exists('README.rst'):
     os.remove('README.rst')
 
 os.system('pandoc -s README.md --from markdown --to rst -s -o README.rst')
 
+
 if os.path.exists('README.rst'):
     print('Readme generated')
-    os.system("python setup.py register sdist upload")
+    os.system('python setup.py sdist')
+    os.system('python setup.py bdist_wheel --universal')
+    os.system("twine upload dist/*")
+    # Clean
+    shutil.rmtree('dist')
     os.remove('README.rst')
 
 print('All done')
