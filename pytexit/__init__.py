17500b0fc8f2ddfbf8726412480485747498cdbd
Erwan <erwan.pannier@centralesupelec.fr>
2018-04-24 14:41:19 +0200

    Read version number from file

--- pytexit/__init__.py
+++ pytexit/__init__.py
@@ -7,3 +7,12 @@ from __future__ import absolute_import
 
 from .core import *
 from .pytexit import py2tex, for2py
+
+def __get_version__():
+    from os.path import join, dirname
+    # Read version number from file
+    with open(join(dirname(__file__), '__version__.txt')) as version_file:
+        __version__ = version_file.read().strip()
+    return __version__
+
+__version__ = __get_version__()
