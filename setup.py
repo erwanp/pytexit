ccb9d0f12e4cae1bf43833f9d106938edd538a6a
Anurag Kumar <mailanu98@gmail.com>
2021-09-10 19:24:34 +0530

    Update setup.py
    updated classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,10 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     install_requires=[
