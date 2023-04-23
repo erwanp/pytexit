78bb98dffbef6a99872d3244ee1e3920fbf4878e
Wesley Havens <wesleydh85@gmail.com>
2023-04-23 17:05:01 -0500

    Added basic support for unicode "÷" symbol. In this case, "÷" is converted to "/" in the unicode table so that it can be correctly parsed. Also added a test case in test_functions.py to confirm.
    Fixed minor typo in docstring for visit_name()

--- pytexit/core/core.py
+++ pytexit/core/core.py
@@ -17,6 +17,7 @@ unicode_tbl = {
     "β": "beta",
     "χ": "chi",
     "δ": "delta",
+    "÷": "/",
     "ε": "epsilon",
     "γ": "gamma",
     "ψ": "psi",
@@ -301,7 +302,7 @@ class LatexVisitor(ast.NodeVisitor):
         """Special features:
         - Recognize underscripts in identifiers names (default: underscore)
         - Recognize upperscripts in identifiers names (default: ˆ, valid in Python3)
-        Note that using ˆ is not recommanded in variable names because it may
+        Note that using ˆ is not recommended in variable names because it may
         be confused with the operator ^, but in some special cases of extensively
         long formulas with lots of indices, it may help the readability of the
         code
